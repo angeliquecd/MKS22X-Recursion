76deacc294b1695b808715dca237ed493691cbea
angeliquecd <acharles-davis00@stuy.edu>
2019-02-02 13:43:31 -0500

    adding addsum method

--- Recursion.java
+++ Recursion.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
 public class Recursion{
   public static double sqrtreg(double n){//uses loop
     if (n==0) return 0;
@@ -20,12 +23,25 @@ public class Recursion{
     return sqrthelper(n,(n/guess+guess)/2,tolerance);
   }
 public static int fib(int n){
-  if (n<=1) return n;
-  return helperfib(1,1,1,n);
+  if (n==0) return 0;
+  if (n==1) return 1;
+  return helperfib(1,1,n);
 }
-public static int helperfib(int first,int second,int index,int n){
-  if (index==n) return first;
-  return helperfib(second,first+second,index+1,n);
+public static int helperfib(int first,int second,int n){
+  if (1==n) return first;
+  return helperfib(second,first+second,n-1);
+}
+public static ArrayList<Integer> makeAllSums(int n){
+  ArrayList<Integer> returner = new ArrayList<Integer>();
+  sumsHelper(n,1,1,returner);
+  return returner;
+}
+public static boolean sumsHelper(int n, int current, int partial, ArrayList<Integer> returner){
+  if (current==n) {
+    System.out.println(partial);
+    returner.add(partial);}
+//  if (returner.size()==Math.pow(2,n)) return true;
+  return sumsHelper(n, current+1, partial+current, returner)|| sumsHelper(n,current+1,partial,returner);
 }
   public static void main(String[] args){
     double r = .001;
@@ -38,7 +54,12 @@ public static int helperfib(int first,int second,int index,int n){
     System.out.println("-----\n testing fib: ");
     System.out.println(fib(1));
     System.out.println(fib(5));
+    System.out.println(fib(10));
+    System.out.println(fib(30));
+    System.out.println(fib(35));
+    System.out.println(fib(48));
     System.out.println(fib(49));
-
+    System.out.println("-----\n testing makeallsums: ");
+    System.out.println(makeAllSums(3));
   }
 }
